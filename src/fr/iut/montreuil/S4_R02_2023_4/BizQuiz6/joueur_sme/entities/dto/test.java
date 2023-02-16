c724a10f137bc721dc6862c54ea2d78773efd57f
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2023-02-16 15:51:50 +0100

    ajout de fichier test pour affichage de package

---
+++ src/fr/iut/montreuil/S4_R02_2023_4/BizQuiz6/joueur_sme/entities/dto/test.java
@@ -0,0 +1,5 @@
+package fr.iut.montreuil.S4_R02_2023_4.BizQuiz6.joueur_sme.entities.dto;
+
+public class test {
+
+}
---
+++ test/fr/iut/montreuil/S4_R02_2023_4/BizQuiz6_sme/impl/test.java
@@ -0,0 +1,5 @@
+package fr.iut.montreuil.S4_R02_2023_4.BizQuiz6_sme.impl;
+
+public class test {
+
+}
